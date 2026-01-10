b49b47334758ffdc653b8c22657042f317b47af7
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2026-01-10 12:12:36 +0530

    backend cors db and env.example added

--- apps/backend/src/server.js
+++ apps/backend/src/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const corsMiddleware = require("./configs/cors");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 5001;
 
+app.use(corsMiddleware);
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -13,4 +15,5 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
   console.log(`✅ Local Backend URL: ${process.env.BACKEND_LOCAL_URL}`);
+  console.log(`✅ Deployed Backend URL: ${process.env.BACKEND_SERVER_URL}`);
 });
